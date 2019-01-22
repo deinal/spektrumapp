b65364b2a7ab0dc5be6fa36234f128196fdc9cc5
Daniel Holmberg <daniel.holmberg97@gmail.com>
2019-01-22 13:43:34 +0200

    News replace function edit

--- src/News/newsitem.js
+++ src/News/newsitem.js
@@ -3,12 +3,11 @@ import { View, Text } from 'react-native';
 import styles from './../Styles/index'
 
 export default class Todo extends React.PureComponent {
-
     render() {
         return (
             <View style={{ flex: 1, paddingTop: 20 }}>
-                <Text style={styles.food}>{this.props.title}{"\n"}</Text>
-                <Text style={{color: 'black', margin: 5}}>{this.props.content}</Text>
+                <Text style={styles.food}>{this.props.title.replace(/\\|    /g,"\n\n")}{"\n"}</Text>
+                <Text style={{color: 'black', margin: 5}}>{this.props.content.replace(/\\|    /g,"\n\n")}</Text>
             </View>
         );
     }
